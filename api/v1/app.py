cf1525143f61cb9bc065eddf7d5fd50085cd33e0
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-09-30 11:36:54 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 '''Contains a Flask web application API.'''
 
-from flask import Flask, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
-from os import getenv
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def downtear(self):
     '''Status of your API'''
     storage.close()
 
+
 @app.errorhandler(404)
 def error_404(error):
-    '''Handles the 404 error code.'''
-    return jsonify(error='Not found'), 404
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
